7e75f75ce1cb1d2e38a6ccceb3bc92fb314f29e4
rtfp0102 <rtfp0102@gmail.com>
2019-09-17 23:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function logWhisper (string) {
 }
 function sayHiToGrandma (string) {
   if (string === "hello") {
-    return "I can't hear you"
+    return "I can't hear you!"
+  } else if (string === "HELLO") {
+    return "YES INDEED"
   }
+  
 }
