aec909284bd6074bd34a23a99a9484495731d8aa
rtfp0102 <rtfp0102@gmail.com>
2019-09-18 00:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma (string) {
   if (string === "hello") {
     return "I can't hear you!"
   } else if (string === "HELLO") {
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
